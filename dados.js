09d790c5d9c5f93ff37db8d2f47a78278024ac0c
manoelmozzer <manoelmozzer@hotmail.com>
2024-06-24 01:14:46 -0300

    Initial commit

---
+++ dados.js
@@ -0,0 +1,114 @@
+function pegarDados() {
+
+// Identificação
+processo = document.getElementById('processo');
+  processo = processo.value;
+exequente = document.getElementById('exequente');
+  exequente = exequente.value;
+executado = document.getElementById('executado');
+  executado = executado.value;
+identificacao = [processo, exequente, executado];
+
+// Saldo em conta judicial
+conta = document.getElementById('conta');
+  conta = conta.value;
+saldo = document.getElementById('saldo');
+  saldo = saldo.value;
+  // Converte para float antes de passar para script decimal
+  // Gera erro se não fizer a conversão
+  saldo = parseFloat(saldo);
+  saldo = new Decimal(saldo);
+
+// Servidor e Cargo
+servidor = document.getElementById('servidor');
+  servidor = servidor.value;
+cargo = document.getElementById('cargo');
+  cargo = cargo.value;
+
+// Tabela
+//// Inicializações
+table = document.getElementById('table');
+favorecido = [];
+valor = [];
+valor_liberado = [];
+valor_total_liberado = [];
+percentagem = [];
+percentagem_total = [];
+tabela = [];
+
+//// Construir arrays
+for (var r = 1, n = table.rows.length; r < n; r++) {
+  for (var c = 2, m = table.rows[r].cells.length; c < m; c++) {
+    favorecido.push(table.rows[r].cells[1].children[0].value);
+    valor.push(table.rows[r].cells[2].children[0].value);
+  }
+}
+
+//// Converter array para número e decimais
+function toNumber(v) {
+  v = v.replace(/,/, '.');
+  v = parseFloat(v);
+  return new Decimal(v);
+}
+valor = valor.map(toNumber);
+
+//// Somar os valores
+// valor_total = valor_total((a,b) => a + b, 0);
+valor_total = valor.reduce((a,b) => Decimal.add(a, b), 0);
+
+//// Cálculos
+var resto = new Decimal(0.00);
+for (i=0; i<valor.length; i++) {
+  // Percentual
+  // percentagem.push(valor[i]/valor_total);
+  percentagem.push(Decimal.div(valor[i],valor_total));// valor[i] / valor_total
+  // Distribuição
+  //// Somente valor correto
+  divisao = Decimal.mul(saldo, percentagem[i]);// saldo * percentagem[i]
+  divisao_trunc = divisao.mul(100).trunc().div(100)// Math.trunc(divisao *100) / 100
+  resto = resto.add(divisao.sub(divisao_trunc));// resto + ((saldo*percentagem[i]) - divisao_trunc)
+  valor_liberado.push(divisao_trunc);
+}
+
+//// Distribuição dos centavos restantes (espúrios)
+if (resto !== 0) {
+  // Arrendonda o resto: integra as mili frações perdidas nas últimas casas decimais
+  resto = resto.mul(100).round().div(100);// Math.round(resto * 100) / 100;
+  i = 0;
+  while (resto > 0.00 ) {
+    valor_liberado[i] = valor_liberado[i].add(0.01);// valor_liberado[i] + 0.01;
+    resto = resto.sub(0.01);// resto -= 0.01;
+    i++
+  }
+}
+
+//// Valores totais
+valor_total_liberado = valor_liberado.reduce((a,b) => Decimal.add(a, b), 0);
+percentagem_total = percentagem.reduce((a,b) => Decimal.add(a, b), 0);
+
+//// Conferência dos totais
+if (saldo.toString() != valor_total_liberado.toString() || resto.toString() != 0) {
+  alert("Há campos de valores sem preencher ou preenchidos inadequadamente!")
+}// else {console.log('OK')}
+
+//// Formatação
+function formatarDinheiro(v) {
+  // return new Intl.NumberFormat('pt-br', {minimumFractionDigits: 2, maximumFractionDigits: 2,}).format(v);
+  return new Intl.NumberFormat('pt-br', {style: 'currency', currency: 'BRL', minimumFractionDigits: 2, maximumFractionDigits: 2,}).format(v);
+}
+valor_liberado = valor_liberado.map(formatarDinheiro);
+valor_total_liberado = formatarDinheiro(valor_total_liberado);
+saldo = formatarDinheiro(saldo);
+
+function formatarPercentagem(v) {
+  return Number(v).toLocaleString('pt-br',{style: 'percent', minimumFractionDigits:6});
+}
+percentagem = percentagem.map(formatarPercentagem);
+percentagem_total = formatarPercentagem(percentagem_total);
+
+//// Contruir array da tabela
+for (i=0; i<valor.length; i++) {
+  tabela.push([favorecido[i], valor_liberado[i], percentagem[i]]);
+}
+
+}
---
+++ pdf.js
@@ -0,0 +1,154 @@
+function pdf(){
+  window.jsPDF = window.jspdf.jsPDF; // add this line of code
+
+pegarDados();
+
+const doc = new jsPDF({
+  // orientation: "landscape",
+  // Tamanho A4
+  // unit: "mm",
+  // format: [297, 210],
+  format: 'a4',
+  unit: "mm",
+});
+
+// Padrão: helvetica
+// console.log(doc.getFont());
+doc.setFont("helvetica");
+// Padrão: 16
+// console.log(doc.getFontSize());
+doc.setFontSize(14);
+
+// Parâmetro x para centralizar texto
+meio = (doc.internal.pageSize.width / 2);
+
+// Margens
+margem_superior = 20 // 2 cm
+margem_inferior = 20 // 2 cm
+margem_esquerda = 15 // 1,5 cm
+margem_direita = 15 // 1,5 cm
+
+// Controle de linhas
+tamanho_linha = 6 // 0,6 cm
+function linha(n) {
+  return margem_superior+(n*tamanho_linha);
+}
+var linha_atual = 0;
+
+// Cabeçalho
+doc.setFont("helvetica", "bold");
+cabecalho = [
+  "2ª VARA DO TRABALHO DE PATO BRANCO",
+  "Rua Paraná, nº 1.547 - Bairro Sambugaro",
+  "85.801-090 - Pato Branco - PR",
+  "e-mail: vdt01pbc@trt9.jus.br"];
+
+for (let i = 0; i < cabecalho.length; i++) {
+  doc.text(cabecalho[i], meio, linha(i+1), "center");
+}
+
+// Contagem de linhas e Espaços em branco
+var pular_linhas = 4
+var bloco_anterior = cabecalho.length
+var linha_atual = linha_atual + bloco_anterior + pular_linhas
+
+// Identificação
+doc.setFont("helvetica", "normal");
+identificacao_texto = [
+  "Processo: ",
+  "Exequente: ",
+  "Executado: "];
+identificacao;// Origem 'dados.js'
+
+for (let i = 0; i < identificacao_texto.length; i++) {
+  doc.text(identificacao_texto[i] + identificacao[i], margem_esquerda, linha(linha_atual+i));
+}
+
+// Contagem de linhas e Espaços em branco
+var pular_linhas = 3
+var bloco_anterior = identificacao_texto.length
+var linha_atual = linha_atual + bloco_anterior + pular_linhas
+
+// Saldo em conta judicial
+doc.setTextColor("red");
+conta_texto = "Saldo em conta judicial 4182-042-";
+conta;// Origem 'dados.js'
+saldo;// Origem 'dados.js'
+doc.text(conta_texto + conta, margem_esquerda, linha(linha_atual));
+// doc.text("R$ " + saldo, doc.internal.pageSize.width-margem_direita, linha(linha_atual), "right");
+doc.text(saldo, doc.internal.pageSize.width-margem_direita, linha(linha_atual), "right");
+
+// Contagem de linhas e Espaços em branco
+var pular_linhas = 2
+var bloco_anterior = 1
+var linha_atual = linha_atual + bloco_anterior + pular_linhas
+
+
+// Tabela
+head = [['Favorecido', 'Valor', 'Percentual']]
+body = tabela
+// foot = [['Total', 'R$ ' + valor_total_liberado, percentagem_total]]
+foot = [['Total', valor_total_liberado, percentagem_total]]
+
+doc.autoTable({
+  startY: linha(linha_atual),
+  margin: {left: margem_esquerda, right: margem_direita},
+  head: head,
+  body: body,
+  foot: foot,
+  styles: {
+    font: 'helvetica',
+    fontSize: number = 12,
+  },
+  headStyles: {fillColor: [64,64,64]},// Preenchimento preto 25% transparência
+  footStyles: {fillColor: [64,64,64]},// Preenchimento preto 25% transparência
+  bodyStyles: {textColor: 0},// Letra preta
+  // Alinhamento manual de célular do cabeçalho e rodapé
+  // https://stackoverflow.com/questions/66022870/align-text-left-using-jspdf-jspdf-auto-table
+  didParseCell: data => {
+    if (data.cell.section === 'head' || data.cell.section === 'foot') {
+      switch (data.column.index) {
+          case 1:
+          case 2:
+              data.cell.styles.halign = 'right';
+              // data.cell.styles.overflow = 'visible';// Não quebra a linha, mesmo que invada a próxima cédula
+              data.cell.styles.cellWidth = 'wrap';// Não quebra a linha
+        }
+      }
+    },
+  columnStyles: {
+    // 0: {minCellWidth: 110},
+    1: {halign: 'right'},
+    2: {halign: 'right', cellWidth:30},
+  },
+})
+
+// Posição Y final da última tabela
+// console.log(doc.lastAutoTable.finalY);
+
+// Contagem de linhas e Espaços em branco
+var pular_linhas = 3
+var linha_atual = Math.trunc((doc.lastAutoTable.finalY-margem_superior)/tamanho_linha)+1
+var linha_atual = linha_atual + pular_linhas
+
+// Data e local
+doc.setTextColor("black");
+data_atual = new Date();
+data_atual = data_atual.toLocaleDateString('pt-BR',{day: 'numeric', month: 'long', year: "numeric"});
+doc.text("Pato Branco, " + data_atual, meio, linha(linha_atual), "center");
+
+
+// Contagem de linhas e Espaços em branco
+var pular_linhas = 2
+var bloco_anterior = 1
+var linha_atual = linha_atual + bloco_anterior + pular_linhas
+
+// Servidor e função
+doc.text(servidor, meio, linha(linha_atual), "center");
+doc.text(cargo, meio, linha(linha_atual+1), "center");
+
+// https://stackoverflow.com/questions/17739816/how-to-open-generated-pdf-using-jspdf-in-new-window
+doc.save("Demonstrativo de Liberação de Valores.pdf");
+// doc.output('dataurlnewwindow');
+
+}
